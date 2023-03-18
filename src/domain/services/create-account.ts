fada8db91f3121ba35c9dcf5f0a0155552244428
Silvio - Windows <silvio.gabrielgt2000@hotmail.com>
2023-03-18 19:47:33 -0300

    updated create user mutation

--- src/domain/services/create-account.ts
+++ src/domain/services/create-account.ts
@@ -7,15 +7,10 @@ export class CreateAccountService {
   public async create(
     param: CreateAccountService.Params
   ): Promise<CreateAccountService.Result> {
-    const validateEmail = await this.usersRepository.findByEmail(param.email);
     const validatePhone = await this.usersRepository.findByPhone(param.phone);
 
     if (validatePhone) {
       throw new Error("Telefone já cadastrado!");
-    }
-
-    if (validateEmail) {
-      throw new Error("Email já cadastrado!");
     }
 
     return this.usersRepository.create({
@@ -28,7 +23,7 @@ export namespace CreateAccountService {
   export type Params = {
     id: string;
     name: string;
-    email: string;
+    email?: string;
     phone: string;
   };
   export type Result = User;
