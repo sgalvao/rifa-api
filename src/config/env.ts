5ad52545268b9b633ba863525170f0917c677767
Ruan Silva <ruanrafaelleite@gmail.com>
2023-04-17 00:37:44 -0300

    Added api oauth-token from mercado pago

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,10 @@
 export default {
 	port: process.env.PORT || 9000,
 	jwtSecret: process.env.JWT_SECRET,
+	host: process.env.HOST,
+}
+
+export const MERCADO_PAGO = {
+	CLIENT_ID: process.env.MERCADO_PAGO_CLIENT_ID,
+	CLIENT_SECRET: process.env.MERCADO_PAGO_CLIENT_SECRET,
 }
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,18 +1,12 @@
 import { VerifyPaymentStatusService } from "@/domain/services"
-import { MercadoPagoProvider } from "@/infra/providers/mercado-pago"
+import mercadoPago from "@/infra/providers/mercado-pago"
 import { PaymentIntentRepository, RifaRepository } from "@/infra/repositories"
 import { Express } from "express"
 
 const makePaymentVerifyStatus = () => {
 	const rifaRepository = new RifaRepository()
 	const paymentIntentRepository = new PaymentIntentRepository()
-	const mercadoPagoProvider = new MercadoPagoProvider()
-	const verifyPaymentStatus = new VerifyPaymentStatusService(
-		rifaRepository,
-		paymentIntentRepository,
-		mercadoPagoProvider
-	)
-
+	const verifyPaymentStatus = new VerifyPaymentStatusService(rifaRepository, paymentIntentRepository, mercadoPago)
 	return verifyPaymentStatus
 }
 
@@ -20,5 +14,18 @@ export const setupRoutes = (app: Express): void => {
 	app.get("/api/payment_pix", (req, res) => {
 		makePaymentVerifyStatus().verify()
 		return res.send(200)
+	})
+
+	app.get("/api/oauth", async (req, res) => {
+		const { code } = req.query
+
+		if (!code) {
+			console.error("code not provided")
+		}
+
+		const { access_token, refresh_token, expires_in } = await mercadoPago.getOauthTokens(code as string)
+		mercadoPago.setTokens(access_token, refresh_token, new Date(expires_in))
+
+		res.send()
 	})
 }
