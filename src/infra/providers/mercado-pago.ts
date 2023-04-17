942c08f4ffe0032ffdbb81adb4e27ea12d5b961d
Ruan Silva <ruanrafaelleite@gmail.com>
2023-04-17 00:37:25 -0300

    implemented mercado pago provider as singleton

--- src/infra/providers/mercado-pago.ts
+++ src/infra/providers/mercado-pago.ts
@@ -1,13 +1,81 @@
 import MercadoPago from "mercadopago"
 import axios, { Axios } from "axios"
+import env, { MERCADO_PAGO } from "@/config/env"
 
-export class MercadoPagoProvider {
-	axios: Axios = axios.create({
-		baseURL: "https://api.mercadopago.com",
-	})
+let instance: MercadoPagoProvider = null
 
-	connect() {
-		MercadoPago.configure({ access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN })
-		return MercadoPago
+type TokensResponse = {
+	access_token: string
+	refresh_token: string
+	expires_in: string
+}
+interface IMercadoPagoProvider {
+	getInstance: () => MercadoPagoProvider
+	configure: (accessToken: string) => void
+	getOauthTokens: (code: string) => Promise<TokensResponse>
+	refreshTokens: (refreshToken: string) => Promise<TokensResponse>
+	setTokens: (accessToken: string, refreshToken: string, expiresIn: Date) => void
+}
+
+export class MercadoPagoProvider implements IMercadoPagoProvider {
+	private readonly axios: Axios
+	private _refreshToken: string = null
+	private expiresIn: Date = null
+
+	constructor() {
+		if (instance) {
+			return instance
+		}
+
+		this.axios = axios.create({
+			baseURL: "https://api.mercadopago.com",
+		})
+		instance = this.getInstance()
+		return instance
+	}
+
+	readonly sdk: typeof MercadoPago = MercadoPago
+
+	getInstance = () => {
+		return this
+	}
+
+	configure(accessToken: string) {
+		this.sdk.configure({
+			access_token: accessToken,
+		})
+	}
+
+	getOauthTokens = async (code: string) => {
+		const { data } = await this.axios.post("/oauth/token", {
+			client_secret: MERCADO_PAGO.CLIENT_SECRET,
+			client_id: MERCADO_PAGO.CLIENT_ID,
+			grant_type: "authorization_code",
+			redirect_uri: `${env.host}/api/oauth`,
+			code,
+		})
+
+		return data
+	}
+
+	refreshTokens = async (refreshToken: string) => {
+		const { data } = await this.axios.post("/oauth/token", {
+			client_secret: MERCADO_PAGO.CLIENT_SECRET,
+			client_id: MERCADO_PAGO.CLIENT_ID,
+			grant_type: "refresh_token",
+			refresh_token: refreshToken,
+		})
+
+		return data
+	}
+
+	setTokens = async (accessToken: string, refreshToken: string, expiresIn: Date) => {
+		this.sdk.configure({
+			access_token: accessToken,
+		})
+		this._refreshToken = refreshToken
+		this.expiresIn = expiresIn
 	}
 }
+
+export default new MercadoPagoProvider()
